1646ccb908c80e08f0fb8a26f972441b69dd97d2
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-06-20 23:10:13 -0300

    Finalização da estilização da calculadora, verificação de cálculos e resultados mostrados em tela.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const homensInput = document.getElementById('homens');
-const mulheresInput = document.getElementById('mulheres');
-const criancasInput = document.getElementById('criancas');
+const qtdHomensInput = document.getElementById('homens');
+const qtdMulheresInput = document.getElementById('mulheres');
+const qtdCriancasInput = document.getElementById('criancas');
 const calcularButton = document.getElementById('calcular');
 
 const carneBovinaOutput = document.getElementById('carne-bovina');
@@ -10,9 +10,9 @@ const refrigeranteOutput = document.getElementById('refrigerante');
 const cervejaOutput = document.getElementById('cerveja');
 
 calcularButton.addEventListener('click', () => {
-  const homens = parseInt(homensInput.value);
-  const mulheres = parseInt(mulheresInput.value);
-  const criancas = parseInt(criancasInput.value);
+  const homens = parseInt(qtdHomensInput.value);
+  const mulheres = parseInt(qtdMulheresInput.value);
+  const criancas = parseInt(qtdCriancasInput.value);
 
   const carneBovina = ((homens * 500) + (mulheres * 300) + (criancas * 200)) / 1000;
   const frango = ((homens * 200) + (mulheres * 200) + (criancas * 100)) / 1000;
@@ -20,9 +20,9 @@ calcularButton.addEventListener('click', () => {
   const refrigerante = ((homens * 300) + (mulheres * 400) + (criancas * 200)) / 1000;
   const cerveja = ((homens * 800) + (mulheres * 500)) / 1000;
 
-  carneBovinaOutput.textContent = carneBovina + 'Kg';
-  frangoOutput.textContent = frango + 'Kg';
-  linguiçaOutput.textContent = linguiça + 'Kg';
-  refrigeranteOutput.textContent = refrigerante + 'l';
-  cervejaOutput.textContent = cerveja + 'l';
+  carneBovinaOutput.textContent = carneBovina + ' kg';
+  frangoOutput.textContent = frango + ' kg';
+  linguiçaOutput.textContent = linguiça + ' kg';
+  refrigeranteOutput.textContent = refrigerante + ' l';
+  cervejaOutput.textContent = cerveja + ' l';
 });
